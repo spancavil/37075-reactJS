cfe141d7bfd2ae07243e123f9280cff4f4fcb5c0
spancavil <soancavil@gmail.com>
2022-06-08 11:16:03 -0300

    Desarrollo clase II

---
+++ Clase 2 - Instalación/class.js
@@ -0,0 +1,39 @@
+//Destructuring de objetos
+const persona1 = {
+    nombre: "Oscar",
+    apellido: "De la Hoya",
+    edad: 45,
+    estadoCivil: "soltero",
+    cantidadCampeonatos: 30,
+    nacionalidad: "mexicanestadounidense"
+}
+
+// console.log(persona1.nombre)
+
+const {nombre, estadoCivil, cantidadCampeonatos: campeonatos} = persona1;
+
+// console.log(nombre);
+// console.log(campeonatos);
+// console.log(cantidadCampeonatos);
+
+//Destructuring de arrays
+const personajes = ["nautilus", "amumu", "master yi", "ryze", "kyle"]
+
+console.log(personajes[3])
+
+const [primerPersonaje, segundoPersonaje, ...restoPersonajes] = personajes;
+
+console.log(primerPersonaje);
+console.log(restoPersonajes);
+
+//Spread operator
+const persona2 = {
+    ...persona1,
+    sueldo: '23K USD'
+}
+
+const personajesCopia = [...personajes]
+
+console.log(persona2);
+
+//Función map, find
