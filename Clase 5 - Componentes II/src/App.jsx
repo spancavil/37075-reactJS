f328ca5a935f71fe530c9887474286535d6bcebb
spancavil <soancavil@gmail.com>
2022-06-20 12:07:08 -0300

    Desarrollo clase 5

--- Clase 5 - Componentes II/src/App.jsx
+++ Clase 5 - Componentes II/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 // import Input from './components/Input';
 import NavBar from './components/NavBar';
@@ -5,12 +6,23 @@ import ItemListContainer from './containers/ItemListContainer';
 
 function App() {
 
+  const [containerVisible, setContainerVisible] = useState(true)
+
+  const onHideContainer = () => {
+    setContainerVisible(false)
+  }
+
   return (
     <div className='container'>
-      <NavBar/>
-      <ItemListContainer greeting="Hola Coders!">
-        <h2>Este es el item list container</h2>
-      </ItemListContainer>
+      <NavBar />
+      {containerVisible ?
+        <ItemListContainer greeting="Hola Coders!">
+          <h2>Este es el item list container</h2>
+        </ItemListContainer>
+        :
+        null
+      }
+      <button onClick={onHideContainer}>Desmontar ItemListContainer</button>
       {/* <Input valorPlaceholder={"Hola"} title={"cómo estás?"}/>
       <Input valorPlaceholder={"Hola"} title={"cómo estás?"}/> */}
     </div>
---
+++ Clase 5 - Componentes II/src/components/ItemCount/index.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const ItemCount = ({handleAdd, initialStock}) => {
+
+    const [count, setCount] = useState(0)
+
+    const onAdd = () => {
+
+    } 
+
+    const onDecrement = () => {
+
+    }
+
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={onAdd}>+</button>
+            <button onClick={onDecrement}>-</button>
+            <button onClick={handleAdd}> Agregar al carrito</button>
+        </div>
+    )
+}
+
+export default ItemCount
